86c76ea7a07d9776185adfb7c6b1bdbfc38fc840
Vidar Aune Westrum <vidar@dolittle.com>
2018-02-15 15:44:55 +0100

    Restructured the package and added documentation to it.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,12 +1,16 @@
-const CodeMirror = require("codemirror");
+const CodeMirror = require('codemirror');
 require('codemirror/mode/htmlmixed/htmlmixed');
 require('codemirror/mode/sass/sass');
 
+const hamburger = document.getElementById('main-nav-trigger');
+const mainNavigation = document.querySelectorAll('.main-navigation')[0];
+const mainBody = document.getElementById('main-body');
+
 function generateCodePreviews() {
-    var previewBlocks = document.querySelectorAll("div.preview");
+    var previewBlocks = document.querySelectorAll('div.preview');
     previewBlocks.forEach(function (element) {
         var elementCode = element.innerHTML;
-        var siblingCodeBlock = element.parentElement.querySelector("textarea");
+        var siblingCodeBlock = element.parentElement.querySelector('textarea');
         var codemode = siblingCodeBlock.dataset.codemode;
 
         siblingCodeBlock.value = elementCode.replace(/  /g, '').replace(/^\s+|\s+$/g, '');
@@ -19,10 +23,10 @@ function generateCodePreviews() {
 }
 
 function makeCodeLookPretty() {
-    var codeBlocks = document.querySelectorAll("textarea.codemirror-me");
+    var codeBlocks = document.querySelectorAll('textarea.codemirror-me');
     codeBlocks.forEach(function (element) {
         var codeMode = element.dataset.codemode;
-        
+
         element.value = element.value.replace(/^\s+|\s+$/g, '');
         CodeMirror.fromTextArea(element, {
             mode: codeMode,
@@ -32,6 +36,21 @@ function makeCodeLookPretty() {
     });
 }
 
+var toggleActiveMenuClass = function (element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    }
+    else {
+        element.classList.add(className);
+    }
+}
+
 
 generateCodePreviews();
 makeCodeLookPretty();
+
+hamburger.addEventListener('click', function (event) {
+    toggleActiveMenuClass(event.target, 'is-active');
+    toggleActiveMenuClass(mainNavigation, 'expanded');
+    toggleActiveMenuClass(mainBody, 'menu-active');
+});
