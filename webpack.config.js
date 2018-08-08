7e63eaa0ee4482d2f4a7824c43ae836166a0547c
Vidar Aune Westrum <vidar@dolittle.com>
2018-08-08 11:12:21 +0200

    added components to the kit and added acrylic component

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path');
-const configParts = require("./webpack.parts");
+const configParts = require('./webpack.parts');
 
-
-module.exports = [{
-    entry: ['./app/scripts/index.js', './app/styles/style.scss'],
-    output: {
-        filename: 'bundle.[hash:5].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            configParts.loaders.htmlLoader,
-            configParts.loaders.sassLoader,
-            configParts.loaders.svgLoader,
-            configParts.loaders.imageLoader
-        ]
-    },
-    plugins: [
-        configParts.plugins.cleanDistFolder,
-        configParts.plugins.buildHtmlIndex,
-        configParts.plugins.buildHtmlSettings,
-        configParts.plugins.buildHtmlTools,
-        configParts.plugins.buildHtmlGeneric,
-        configParts.plugins.buildHtmlElements,
-        configParts.plugins.buildHtmlObjects,
-        configParts.plugins.sassBuilder
-    ],
-    devServer: {
-        contentBase: './dist',
-        port: 1337,
-        open: true
+module.exports = [
+    {
+        entry: ['./app/scripts/index.js', './app/styles/style.scss'],
+        output: {
+            filename: 'bundle.[hash:5].js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        module: {
+            rules: [
+                configParts.loaders.htmlLoader, 
+                configParts.loaders.sassLoader, 
+                configParts.loaders.svgLoader, 
+                configParts.loaders.imageLoader
+            ]
+        },
+        plugins: [
+            configParts.plugins.cleanDistFolder,
+            configParts.plugins.buildHtmlIndex,
+            configParts.plugins.buildHtmlSettings,
+            configParts.plugins.buildHtmlTools,
+            configParts.plugins.buildHtmlGeneric,
+            configParts.plugins.buildHtmlElements,
+            configParts.plugins.buildHtmlObjects,
+            configParts.plugins.buildHtmlComponents,
+            configParts.plugins.sassBuilder
+        ],
+        devServer: {
+            contentBase: './dist',
+            port: 1337,
+            open: true
+        }
     }
-}];
+];
